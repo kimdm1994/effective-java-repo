b1bfe73e1b3b2cf6b8ee1a6456671a9684f1725f
devkim <kdm8939@naver.com>
2022-04-04 21:33:04 +0900

    advanced factory capter1

--- effective-java-part1/src/main/java/com/me/eastern/effective/chapter01/item01/Settings.java
+++ effective-java-part1/src/main/java/com/me/eastern/effective/chapter01/item01/Settings.java
@@ -23,14 +23,41 @@ public class Settings {
     private Settings() {
     }
 
-    ;
+    /*
+     * 생성자 vs 정적 팩터리 메서드 비교
+     * */
+    public Settings(boolean useAutoSteering, boolean useABS, Difficulty difficulty) {
+        this.useAutoSteering = useAutoSteering;
+        this.useABS = useABS;
+        this.difficulty = difficulty;
+    }
 
     private static final Settings SETTINGS = new Settings();
 
     /*
      * 오로지 정적 팩터리 메서드를 통해 가져갈 수 있음.
+     * 매번 팩터리 안에서 새로 만들어 사용할 떄는 newInstance, createInstance 네이밍 사용
      * */
     public static Settings newInstance() {
         return SETTINGS;
     }
+
+    /*
+     * 매개변수를 받아서 무언가를 만들 때 of라는 네이밍을 패턴을 사용한다.
+     * */
+    public static Settings of(boolean useABS) {
+        return SETTINGS;
+    }
+
+    /*
+     * 미리 만들어져 있는 인스턴스를 가져올 때는 getInstance라는 네이밍 패턴을 사용한다.
+     * */
+    public static Settings getInstance() {
+        return SETTINGS;
+    }
+
+    // public static void main(String[] args) {
+    //     List<String> list = new ArrayList<>();
+    //     List.of("dongmin", "eastern");
+    // }
 }
